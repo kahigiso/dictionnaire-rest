0d602af5cdbbf8478fdefeb9472dd3aa2abf33ab
kahigiso <jkahigiso@gmail.com>
2016-01-02 19:04:01 -0500

    first commit

---
+++ src/main/java/net/francais/mashi/DictionnaireRestApplication.java
@@ -0,0 +1,12 @@
+package net.francais.mashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionnaireRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionnaireRestApplication.class, args);
+	}
+}
